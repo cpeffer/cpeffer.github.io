f3e70d6e0284a2b9c46366694661a8ba9c31e525
Colton Peffer <cpeffer1@ford.com>
2025-01-15 13:59:10 -0500

    starting to add information about projects

--- src/components/ContactToolTip.tsx
+++ src/components/ContactToolTip.tsx
@@ -4,15 +4,20 @@ import '../styles/ContactToolTip.css';
 const ContactTooltip: React.FC = () => {
   return (
     <div>
-    <div className="container">
-    <span className="tooltiptext">
-      <p>coltpeffer@gmail.com</p>
-      <p>(310) 741-9002</p>
-    </span>
-
-    Contact
-
-    </div>
+      <div className="container">
+        <span className="tooltiptext">
+          <a href="mailto:coltpeffer@gmail.com" style={{
+            textDecoration: 'none',
+            color: 'inherit',
+            cursor: 'pointer'
+          }}
+          >
+            coltpeffer@gmail.com
+          </a>
+          <p>(310) 741-9002</p>
+        </span>
+        Contact
+      </div>
     </div>
   );
 };
