78060581113eebb896558296b837bb32833bdac9
cpeffer <coltpeffer@gmail.com>
2025-02-14 11:04:51 -0500

    changed favicon logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Projects from './components/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Project from './components/Project';
+
 
 function App() {
   return (
